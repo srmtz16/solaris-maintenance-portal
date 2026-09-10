43aa867c2e142a5da76f5722570104cf1b496b01
srmtz16 <smar.americano@gmail.com>
2026-09-09 22:23:17 -0600

    Move system specifications to dedicated client tab

---
+++ app/s/[id]/sistema/page.tsx
@@ -0,0 +1,5 @@
+import { PortalPage } from "../portal-page";
+
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  return <PortalPage params={params} view="sistema" />;
+}
